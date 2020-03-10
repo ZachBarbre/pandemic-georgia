cbce8f069527182d408de23956a77be296f915c8
MichaelBamba <mjbamba2@gmail.com>
2020-03-10 15:17:29 -0400

    Finalized the database schema and created the con file to reach the database

---
+++ models/con.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+
+const pgp = require('pg-promise')({
+    query: function(e){
+        console.log('QUERY:', e.query)
+    }
+});
+const option  = {
+    host:process.env('DB_HOST'),
+    database:process.env('DB_NAME')
+    password:process.env('PASSWORD')
+};
+
+const db=pgp(option);
+
+    module.exports = db
