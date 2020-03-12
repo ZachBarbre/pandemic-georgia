5824982f61da3901fcfe524692f08ba2435fe072
MichaelBamba <mjbamba2@gmail.com>
2020-03-12 14:48:13 -0400

    refined the decks and seperated out the shuffle function

--- cards:deck/startingdeal.js
+++ cards:deck/startingdeal.js
@@ -1,3 +1,13 @@
-require.import playerDeck
-require.import infectDeck
 
+get playerdeck
+let drawn = []
+let infectdrawn = []
+
+
+drawn = playerdeck.pop()
+console.log(drawn)
+
+infectdrawn = infectdeck.pop()
+UPDATE game SET playerdeck = `${playerDeck}`, 
+infectdeck =`${infectDeck}`, discard = `${infectdrawn}` 
+WHERE id = `${req.session.user_id};
--- models/decks.js
+++ models/decks.js
@@ -26,3 +26,7 @@ let playerDeck = [
 ];
 let infectDeck = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let discard = [];
+
+module.export = infectDeck;
+module.export = discard;
+module.export = playerDeck;
