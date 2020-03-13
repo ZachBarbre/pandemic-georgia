c7a49dfb8cd2ce668f2b52c5e3a0895ee4057033
MichaelBamba <mjbamba2@gmail.com>
2020-03-13 13:10:54 -0400

    created an update class

---
+++ models/updateClass.js
@@ -0,0 +1,19 @@
+// basic sql funciton to update the decks on the server
+const db = require("./con");
+const bcrypyt = require("bcryptjs");
+
+class update {
+    constructor (cardspots){
+    this.cardspots = cardspots }
+function updateDs(){
+    UPDATE game SET playerdeck = playerDeck,discard = discard, infectdeck = infectDeck
+    WHERE id = user_id
+} 
+
+
+//needs to be updated with how we are keeping track of player turn
+function updatehand(){
+    UPDATE game SET playerhand = player${}hand
+     WHERE id = user_id
+}}
+module.exports = Teams;
