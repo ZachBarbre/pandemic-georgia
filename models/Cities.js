af510a67e191cbd9815bcf8257953e251a190002
Lockett Pundt <kalifornium@hotmail.com>
2020-03-15 18:21:45 -0400

    added another arguement to take the deck in the cityInit method. also added an empty string to hustory to concatenate on.

--- models/Cities.js
+++ models/Cities.js
@@ -47,7 +47,6 @@ class City {
   }
 
   async removeInfect(city, teamID) {
-    //updates database with new infected total.
     try {
       const value = await this.getInfect(city, teamID);
       //console.log("this is the value:", value);
@@ -57,17 +56,16 @@ class City {
         `UPDATE game SET ${city}infect = $1 FROM teams WHERE game.id = ${teamID};`,
         [bit]
       );
-      // console.log("The response is", response);
-      // console.log("The post is:", post);
-      return post;
+
+      return history;
     } catch (e) {
       return e;
     }
   }
 
-  static async initCity(teamID, playerArray) {
+  static async initCity(teamID, playerArray, deck) {
     const response = await db.one(
-      `INSERT INTO game(id, daltoninfect, blairsvilleinfect, atlantainfect, athensinfect, augustainfect, columbusinfect, maconinfect, savannahinfect, albanyinfect, valdostainfect, player1hand, player2hand, player3hand, player4hand, cure1, cure2, cure3, cure4, playerdeck, infectdeck, player1city, player2city, player3city, player4city, infectrate, playeractions, outbreak, playerturn, actions) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30) RETURNING id`,
+      `INSERT INTO game(id, daltoninfect, blairsvilleinfect, atlantainfect, athensinfect, augustainfect, columbusinfect, maconinfect, savannahinfect, albanyinfect, valdostainfect, player1hand, player2hand, player3hand, player4hand, cure1, cure2, cure3, cure4, playerdeck, infectdeck, player1city, player2city, player3city, player4city, infectrate, playeractions, outbreak, playerturn, actions, history) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31) RETURNING id`,
       [
         teamID,
         0,
@@ -88,7 +86,7 @@ class City {
         false,
         false,
         false,
-        [],
+        deck,
         [],
         playerArray[0],
         playerArray[1],
@@ -98,7 +96,8 @@ class City {
         4,
         0,
         1,
-        4
+        4,
+        ''
       ]
     );
     return response;
