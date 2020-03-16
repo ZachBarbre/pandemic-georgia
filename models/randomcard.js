058d13b686d19b802e1f0b23c4c9f3d57008d77b
MichaelBamba <mjbamba2@gmail.com>
2020-03-16 08:31:14 -0400

    updated randomcard with 3 different states cure nothing and pandemic

--- models/randomcard.js
+++ models/randomcard.js
@@ -2,7 +2,7 @@ function randomCard(cards) {
   return cards[Math.floor(Math.random() * cards.length)];
 }
 
-const cards = [red, green, black, yellow];
+const cards = [cure, cure, cure, nothin, nothin, pandemic];
 console.log(randomCard(cards));
 let card = randomCard(cards);
 module.exports = card;
